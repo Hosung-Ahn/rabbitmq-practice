2f8cc1da2be6d96d8cf3667d2ee1bc2220bb1238
Hosung <an3735297@naver.com>
2024-01-15 16:06:07 +0900

    rabbitmq producer

---
+++ rabbitmq/src/main/java/com/example/rabbitmq/publisher/RabbitMQProducer.java
@@ -0,0 +1,25 @@
+package com.example.rabbitmq.publisher;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RabbitMQProducer {
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void send(String message) {
+        log.info("send message: {}", message);
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, message);
+    }
+}
